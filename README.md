2d57c2538fb91db67704e7c1b8bfafb4049056b9
Avery-Dante <drksknddv@AveryDantesAir.home>
2019-04-23 15:54:52 -0400

    added Drawer to app bar Widget

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A new Flutter project.
 This porject is just me practicing with some new widgets.
 Primarily scrolling grids witin a scrolling list 
 Will implement these widgets into the music player app
-
+Added Draer option to appbar Widget 
 ##Started
 
 This project is a starting point for a Flutter application.
--- lib/main.dart
+++ lib/main.dart
@@ -58,33 +58,28 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      // drawer: Drawer(
-      //   semanticLabel: 'Islands',
-      //   elevation: 16,
-      //   child: ListView(
-      //     controller: ScrollController(
-      //       initialScrollOffset: 2.0,
-      //       keepScrollOffset: true,
-      //     ),
-      //     scrollDirection: Axis.vertical,
-      //     children: <Widget>[
-      //       Text('Islands Will Be Here'),
-      //       FlatButton(child: Text('Menu Button'),
-      //       color: Colors.amberAccent,
-      //       onPressed: () => {_incrementCounter()},
-
-      //     ),
-      //     ],
-
-      //   ),
-      // ),
-      backgroundColor: Color(0x0000000),
-      appBar: AppBar(
-
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          // title: Text(widget.title),
+      drawer: Drawer(
+        semanticLabel: 'Islands',
+        elevation: 16,
+        child: ListView(
+          controller: ScrollController(
+            initialScrollOffset: 2.0,
+            keepScrollOffset: true,
           ),
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Text('Islands Will Be Here'),
+            FlatButton(child: Text('Menu Button'),
+            color: Colors.amberAccent,
+            onPressed: () => {_incrementCounter()},
+
+          ),
+          ],
+
+        ),
+      ),
+      backgroundColor: Color(0x0000000),
+     
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
